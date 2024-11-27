cc950377a0b68015826a67a459e640c899b3fed7
nehha0 <neehhaa0@gmail.com>
2024-11-27 19:52:04 +0530

    done

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -4,11 +4,12 @@ import './App.css'
 import Hero from './assets/component/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
       <Hero/>
+      {/* <div>hello</div> */}
     </>
   )
 }
